adba02fd67e8c9baad1be26102b256f07a8bc728
joanne_ev <joanne29ev@protonmail.com>
2026-07-02 20:17:27 +0100

    converted functions.py to marimo notebook for easier testing

--- wsl/functions.py
+++ wsl/functions.py
@@ -1,53 +1,80 @@
-import polars as pl
-from urllib.error import URLError
+import marimo
 
-def import_research_data(latest_season:int = 25):
-    dfs: list[pl.DataFrame] =[]
-
-    try: 
-        for i in range(23, (latest_season+1)):
-            df: pl.DataFrame = pl.read_csv(f"https://codeberg.org/joanne_ev/data-science-projects/raw/branch/project/wsl/wsl/data/wsl-20{i}-UTC.csv").with_columns(pl.lit(f"{i}/{i+1}").alias("Season"))
-            dfs.append(df)
-            
-    except URLError as e:
-        print(f'Network error: {e.reason}')
-        
-    except ValueError:
-        print('CSVs are not being read as Polars DataFrames')
-    
-    return pl.concat(dfs)
+__generated_with = "0.23.9"
+app = marimo.App()
 
 
-def import_modelling_player_data(latest_season:int = 25):
-    dfs: list[pl.DataFrame] =[]
+@app.cell
+def _():
+    import polars as pl
+    from urllib.error import URLError
 
-    try: 
-        for i in range(16, (latest_season+1)):
-            df: pl.DataFrame = pl.read_csv(f"https://codeberg.org/joanne_ev/data-science-projects/raw/branch/project/wsl/wsl/data/female_players_{i}.csv", infer_schema_length=0).with_columns(pl.lit(f"{i-1}/{i}").alias("FIFA/EA Season"))
-            dfs.append(df)
-            
-    except URLError as e:
-        print(f'Network error: {e.reason}')
-        
-    except ValueError:
-        print('CSVs are not being read as Polars DataFrames')
-    
-    return pl.concat(dfs)
+    return URLError, pl
 
 
-def total_games(team:str, data:pl.DataFrame):
-    games: list[int] = []
+@app.cell
+def _(URLError, pl):
+    def import_research_data(latest_season:int = 25):
+        dfs: list[pl.DataFrame] =[]
 
-    for season in data['Season'].unique().to_list():
-        count: int = (
-            data
-            .filter(
-                pl.col('Season').eq(season),
-                (pl.col('Home Team').eq(team) | pl.col('Away Team').eq(team))
+        try: 
+            for i in range(23, (latest_season+1)):
+                df: pl.DataFrame = pl.read_csv(f"https://codeberg.org/joanne_ev/data-science-projects/raw/branch/project/wsl/wsl/data/wsl-20{i}-UTC.csv").with_columns(pl.lit(f"{i}/{i+1}").alias("Season"))
+                dfs.append(df)
+
+        except URLError as e:
+            print(f'Network error: {e.reason}')
+
+        except ValueError:
+            print('CSVs are not being read as Polars DataFrames')
+
+        return pl.concat(dfs)
+
+    return
+
+
+@app.cell
+def _(URLError, pl):
+    def import_modelling_player_data(latest_season:int = 25):
+        dfs: list[pl.DataFrame] =[]
+
+        try: 
+            for i in range(16, (latest_season+1)):
+                df: pl.DataFrame = pl.read_csv(f"https://codeberg.org/joanne_ev/data-science-projects/raw/branch/project/wsl/wsl/data/female_players_{i}.csv", infer_schema_length=0).with_columns(pl.lit(f"{i-1}/{i}").alias("FIFA/EA Season"))
+                dfs.append(df)
+
+        except URLError as e:
+            print(f'Network error: {e.reason}')
+
+        except ValueError:
+            print('CSVs are not being read as Polars DataFrames')
+
+        return pl.concat(dfs)
+
+    return
+
+
+@app.cell
+def _(pl):
+    def total_games(team:str, data:pl.DataFrame):
+        games: list[int] = []
+
+        for season in data['Season'].unique().to_list():
+            count: int = (
+                data
+                .filter(
+                    pl.col('Season').eq(season),
+                    (pl.col('Home Team').eq(team) | pl.col('Away Team').eq(team))
+                )
+                .shape[0]
             )
-            .shape[0]
-        )
 
-        games.append(count)
+            games.append(count)
 
-    return sum(games)
+        return sum(games)
+
+    return
+
+
+if __name__ == "__main__":
+    app.run()
